9eecfd094281f494ffb6456af7b43cfb4bd392e5
DaffaPutra7 <putradika3604@gmail.com>
2025-04-25 13:51:48 +0700

    buat file login, dan tambah route pada main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1flutter_20220140133/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,10 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/login',
+      routes: {
+        '/login' : (context) => const LoginPage(),
+      },
     );
   }
 }
