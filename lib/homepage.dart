ec74720dc49f7affc07ee01de501148f1e3db249
DaffaPutra7 <putradika3604@gmail.com>
2025-04-27 18:50:08 +0700

    Membuat topbar pada homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1flutter_20220140133/loginpage.dart';
 
 class HomePage extends StatefulWidget {
   final String email;
@@ -14,8 +15,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 20),
+            color: Colors.orange,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/image/amazon.png'),
+                    ),
+                    SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Selamat Datang',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          widget.email,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => LoginPage())
+                    );
+                  }, 
+                  icon: Icon(
+                    Icons.logout, color: Colors.white,
+                  )
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
