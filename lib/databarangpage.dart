260816780ef3c67d3f09d391a16c173577b8e11b
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 19:50:58 +0700

    membuat fungsi selecttanggal, dispode dan initstate

--- lib/databarangpage.dart
+++ lib/databarangpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class DataBarangPage extends StatefulWidget {
   const DataBarangPage({super.key});
@@ -8,7 +10,7 @@ class DataBarangPage extends StatefulWidget {
 }
 
 class _DataBarangPageState extends State<DataBarangPage> {
-   final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _tanggalController = TextEditingController();
   final TextEditingController _jumlahController = TextEditingController();
@@ -26,7 +28,57 @@ class _DataBarangPageState extends State<DataBarangPage> {
     'Tenda': 700000,
     'Sepatu': 350000,
   };
-  
+
+  Future<void> _selectTanggal(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: Colors.orange, 
+              onPrimary: Colors.white, 
+              onSurface: Colors.black
+            ),
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.orange,
+              ),
+            ),
+            dialogTheme: DialogTheme(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24)
+              ),
+            ),
+          ), 
+          child: child!,
+        );
+      },
+    );
+    if (picked != null) {
+      setState(() {
+        _tanggalController.text = DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(picked);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _tanggalController.dispose();
+    _jumlahController.dispose();
+    _hargaController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('id_ID');
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
