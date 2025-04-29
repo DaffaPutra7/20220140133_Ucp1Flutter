fc084fb1f28a6c4a31b5275be9021e29b4bcf435
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 08:10:14 +0700

    Menambah file baru dan dipanggil di homepage

---
+++ lib/datapelangganpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataPelangganPage extends StatefulWidget {
+  const DataPelangganPage({super.key});
+
+  @override
+  State<DataPelangganPage> createState() => _DataPelangganPageState();
+}
+
+class _DataPelangganPageState extends State<DataPelangganPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
