c7cc4041c0ccbbd9f95b9bc5175b2e1b28d69ec8
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 22:17:24 +0700

    Membuat tampilan yang berisikan icon, text Data Berhasil Disimpan dan terdapat column yang berisikan fungsi dan teks yang dikirim dari halaman sebelumnya

--- lib/detaildatabarangpage.dart
+++ lib/detaildatabarangpage.dart
@@ -24,6 +24,76 @@ class DetailDataBarangPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFCF7F5),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.green.shade100, width: 10),
+              ),
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 30,
+                child: Icon(Icons.check, color: Colors.white, size: 40),
+              ),
+            ),
+            SizedBox(height: 30),
+            const Text(
+              'Data Berhasil Disimpan',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  buildInfoRow('Tanggal', tanggal),
+                  buildDivider(),
+                  buildInfoRow('Jenis Transaksi', jenisTransaksi),
+                  buildDivider(),
+                  buildInfoRow('Jenis Barang', jenisBarang),
+                  buildDivider(),
+                  buildInfoRow('Jumlah Barang', jumlah.toString()),
+                  buildDivider(),
+                  buildInfoRow('Jenis Harga Satuan', formatRupiah(harga)),
+                  buildDivider(),
+                  buildInfoRow('Total Harga', formatRupiah(total)),
+                  buildDivider(),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    elevation: 5,
+                  ),
+                  onPressed: () {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  },
+                  child: const Text(
+                    'Selesai',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
