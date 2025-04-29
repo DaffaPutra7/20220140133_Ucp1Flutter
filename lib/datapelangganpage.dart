bbad1c765c24ef3ded01c55fc1fcd059e897b7ed
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 08:35:14 +0700

    Membuat formkey, membuat beberapa controller, dan membuat initstate

--- lib/datapelangganpage.dart
+++ lib/datapelangganpage.dart
@@ -8,6 +8,20 @@ class DataPelangganPage extends StatefulWidget {
 }
 
 class _DataPelangganPageState extends State<DataPelangganPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nohpController = TextEditingController();
+  final TextEditingController _alamatController = TextEditingController();
+  final TextEditingController _provinsiController = TextEditingController();
+  final TextEditingController _posController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
