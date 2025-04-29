6ea412ea387b15a1915eb134fe57075bfe2d4d5f
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 19:40:19 +0700

    Membuat formkey, controller, string, list dan map

--- lib/databarangpage.dart
+++ lib/databarangpage.dart
@@ -8,6 +8,25 @@ class DataBarangPage extends StatefulWidget {
 }
 
 class _DataBarangPageState extends State<DataBarangPage> {
+   final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _tanggalController = TextEditingController();
+  final TextEditingController _jumlahController = TextEditingController();
+  final TextEditingController _hargaController = TextEditingController();
+
+  String? selectedJenisTransaksi;
+  String? selectedJenisBarang;
+
+  final List<String> jenisTransaksi = ['Barang Masuk', 'Barang Keluar'];
+  final List<String> jenisBarang = ['Carrier', 'Sleeping Bag', 'Tenda', 'Sepatu'];
+
+  final Map<String, int> hargaBarang = {
+    'Carrier': 540000,
+    'Sleeping Bag': 250000,
+    'Tenda': 700000,
+    'Sepatu': 350000,
+  };
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
