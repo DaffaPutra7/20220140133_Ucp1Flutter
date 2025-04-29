1385ceb50878e536fdeede6abf19769085d27070
DaffaPutra7 <putradika3604@gmail.com>
2025-04-29 14:46:17 +0700

    Menambah textfromfield untuk Nama Cust, Email, dan No HP dan menghapus _ pada controller

--- lib/datapelangganpage.dart
+++ lib/datapelangganpage.dart
@@ -10,12 +10,12 @@ class DataPelangganPage extends StatefulWidget {
 class _DataPelangganPageState extends State<DataPelangganPage> {
 
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _namaController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _nohpController = TextEditingController();
-  final TextEditingController _alamatController = TextEditingController();
-  final TextEditingController _provinsiController = TextEditingController();
-  final TextEditingController _posController = TextEditingController();
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nohpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController posController = TextEditingController();
 
   @override
   void initState() {
@@ -29,6 +29,107 @@ class _DataPelangganPageState extends State<DataPelangganPage> {
         title: Text('Data Pelanggan', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
         centerTitle: true,
         backgroundColor: Colors.orange,
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Nama Cust', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: namaController,
+                  decoration: InputDecoration(
+                    hintText: 'Nama Customer',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: BorderSide(color: Colors.orange)
+                    ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide(color: Colors.orange)
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(color: Colors.orange)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(color: Colors.orange)
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                          )
+                        ],
+                      )
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('No HP', style: TextStyle(fontWeight: FontWeight.bold)),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: nohpController,
+                            decoration: InputDecoration(
+                              hintText: 'No HP',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(color: Colors.orange)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(color: Colors.orange)
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your phone number';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      )
+                    ),
+                  ],
+                ),
+              ],
+            ), 
+          )
+        ),
       ),
     );
   }
